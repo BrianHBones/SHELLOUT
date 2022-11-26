32e58c8c7546f635272ecf4fa83d8f8a4c302662
BrianHBones <bhartwig123@gmail.com>
2022-11-26 16:33:46 -0600

    hour the fifth one

--- SHELLOUT_NEW/Assets/Scripts/MenuButton.cs
+++ SHELLOUT_NEW/Assets/Scripts/MenuButton.cs
@@ -9,11 +9,22 @@ public class MenuButton : MonoBehaviour
 {
     [SerializeField] float gameStartDelay = 3f;
     public GameObject mainScreen;
+    public GameObject upgradeScreen;
     public Button playButton;
     public Button bonusButton;
 
+
+
     AudioPlayer audioPlayer;
 
+    public void OnButtonExitUpgrade(){
+        mainScreen.SetActive(true);
+        upgradeScreen.SetActive(false);
+    }
+    public void OnButtonUpgrade(){
+        mainScreen.SetActive(false);
+        upgradeScreen.SetActive(true);
+    }
     public void OnButtonStartGame(){
         StartCoroutine(GameStartDelay());
     }
